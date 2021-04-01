238ffd848e0592643cc413f58abdeb9875be0b6b
Kiran Junnarkar <kjunnarkar@yahoo.com>
2021-03-31 19:36:09 -0700

    added see data for a different profile

--- data/seeds/002_cities.js
+++ data/seeds/002_cities.js
@@ -27,6 +27,7 @@ exports.seed = function (knex) {
           livability: '85',
           profile_id: '00ulthapbErVUwVJy4x6',
         },
+
         {
           city: 'Rancho Cucamonga',
           state: 'California',
@@ -38,6 +39,7 @@ exports.seed = function (knex) {
           livability: '85',
           profile_id: '00ulthapbErVUwVJy4x6',
         },
+
         {
           city: 'La Jolla',
           state: 'California',
@@ -48,6 +50,41 @@ exports.seed = function (knex) {
           walkability: '5.0',
           livability: '75',
           profile_id: '00ulthapbErVUwVJy4x6',
+        },
+        {
+          city: 'Tampa',
+          state: 'Florida',
+          rental_price: '1300',
+          crime: 'low',
+          air_quality_index: 'low',
+          diversity_index: '64',
+          walkability: '44',
+          livability: '87',
+          profile_id: 'k43c1w9h0g5q5aoah0x4',
+        },
+        
+        {
+          city: 'Austin',
+          state: 'Texas',
+          rental_price: '1900',
+          crime: 'medium',
+          air_quality_index: 'low',
+          diversity_index: '56',
+          walkability: '66',
+          livability: '87',
+          profile_id: 'k43c1w9h0g5q5aoah0x4',
+        },
+        
+        {
+          city: 'Des Moine',
+          state: 'Iowa',
+          rental_price: '900',
+          crime: 'low',
+          air_quality_index: 'low',
+          diversity_index: '52',
+          walkability: '10',
+          livability: '90',
+          profile_id: 'k43c1w9h0g5q5aoah0x4',
         },
       ]);
     });
